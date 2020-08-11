1ca0bb376c5f9caeb2e8500ae30fa325325d98d2
Made Adidharma Torutama <adidharmatoru@gmail.com>
2020-08-11 11:51:23 +0700

    updated salesActivity functional

--- controllers/SalesQuotationController.js
+++ controllers/SalesQuotationController.js
@@ -8,6 +8,17 @@ exports.index = function(req, res) {
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // If needed
   res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
+  var sql = 'select "DocNum" as "docnum", "DocEntry" as "dockey" from oqut where CANCELED = ' + "'" + 'N' + "'" + ' and "DocStatus" = ' + "'" + 'O' + "'" + ' and "OwnerCode" =  ' + "'" + req.query.uid + "'" + 'order by "DocNum" DESC';
+
+  connection.runQuery(res, sql);
+};
+
+exports.addSQ = function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // If needed
+  res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
   var sql = 'select * from oqut where CANCELED = ' + "'" + 'N' + "'" + ' and "DocStatus" = ' + "'" + 'O' + "'" + ' and "DocNum" LIKE  ' + "'%" + req.query.code + "%'" + ' and "OwnerCode" =  ' + "'" + req.query.uid + "'" + 'order by "DocNum" DESC limit 20';
 
   connection.runQuery(res, sql);
--- routes/salesQuotation.js
+++ routes/salesQuotation.js
@@ -3,5 +3,6 @@ var router = express.Router();
 const SalesQuotationController = require('../controllers/SalesQuotationController');
 
 router.get('/', SalesQuotationController.index);
+router.get('/addSQ', SalesQuotationController.addSQ);
 
 module.exports = router;
