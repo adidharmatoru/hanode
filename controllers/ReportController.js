23f93534b6246d316db064bd7957d0a7733313d0
Faisal Fajar Nursaid <faisalfajarnursaid03@gmail.com>
2021-06-08 11:38:21 +0700

    test3

--- controllers/ReportController.js
+++ controllers/ReportController.js
@@ -266,6 +266,6 @@ exports.alternateItemReport = function (req, res) {
 }
 
 exports.itemReadyForSales = function (req, res) {
-  var sql = 'select "ItemCode", "ItemName", "OnHand", "IsCommited", "OnOrder", U_DM_CAPACITY, U_DM_VOLTAGE, U_DM_PHASE, U_DM_TYPE, U_DM_MODEL, U_DM_STATUS, U_DM_BATTERY, U_DM_COO, U_DM_MEASUREMENT from oitm'
+  var sql = 'select oitm."ItemCode",oitm."ItemName", oitb."ItmsGrpNam", oitm."OnHand", oitm."IsCommited", oitm."OnOrder" from oitm join oitb on oitb."ItmsGrpCod" = oitm."ItmsGrpCod" where oitb."ItmsGrpNam" in (' + req.query.ItmsGrpNam + ')';
   connection.runQuery(res, sql);
 }
