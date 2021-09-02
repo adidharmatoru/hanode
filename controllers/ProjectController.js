48c1cb513821c1a648245796c1f926bac0e54cc1
Faisal Fajar Nursaid <faisalfajarnursaid03@gmail.com>
2021-09-02 17:47:16 +0700

    update BAST

--- controllers/ProjectController.js
+++ controllers/ProjectController.js
@@ -141,7 +141,7 @@ exports.bastMNT = function(req, res, next) {
   res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
   // var sql = 'select * from octr where "ContractID" LIKE  ' + "'%" + req.query.code + "%'" + ' order by octr."ContractID" DESC limit 20';
-  var sql = 'select * from octr Where "CstmrName" LIKE ' + "'%" + req.query.code + "%'" + ' order by octr."ContractID" limit 5';
+  var sql = 'select * from octr Where "CstmrName" LIKE ' + "'%"  + req.query.code + "%'" + ' order by octr."ContractID" limit 5';
 
   connection.runQuery(res, sql);
 };
