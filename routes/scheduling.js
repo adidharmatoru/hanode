1d5cc6538e31003a3a96a325c8a69297a1ebd03b
OpsDelta <faisalfajarnursaid03@gmail.com>
2022-02-02 18:45:18 +0700

    Scheduling

--- routes/scheduling.js
+++ routes/scheduling.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 const SchedulingController = require('../controllers/SchedulingController');
 
-router.get('/serialNum', SchedulingController.scheduling);
+router.get('/serialNum', SchedulingController.schedulingSN);
 
 module.exports = router;
