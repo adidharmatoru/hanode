1aeb0e3ba5c01d7bd271955e89d5ac8ddc5ba9ab
OpsDelta <faisalfajarnursaid03@gmail.com>
2022-02-02 18:47:49 +0700

    scheduling

--- controllers/SchedulingController.js
+++ controllers/SchedulingController.js
@@ -8,7 +8,7 @@ exports.schedulingSN = function(req, res, next) {
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // If needed
   res.setHeader('Access-Control-Allow-Credentials', true); // If needed FORMAT(CAST(T1."U_VIT_TOT" AS DECIMAL(20,6)) , "g16")
 
-    var sql = 'select T0."ContractID" as "ContractID", T1."ItemName" as "ItemName", T1."InternalSN" as "SerialNum", T2."status" as "Status" from octr T0 join ctr1 T1 on T0."ContractID"=T1."ContractID" join oins T2 on T1."InsID" = T2."insID" where T1."InternalSN" in (' + req.body.serial + ') and T1."TermDate" is null and now() < T1."EndDate" and T2."status" = ' + "'" + 'A' + "'" + '';
+    var sql = 'select T0."ContractID" as "ContractID", T1."ItemName" as "ItemName", T1."InternalSN" as "SerialNum", T2."status" as "Status" from octr T0 join ctr1 T1 on T0."ContractID"=T1."ContractID" join oins T2 on T1."InsID" = T2."insID" where "InternalSN" in (' + req.body.serial + ') and T1."TermDate" is null and now() < T1."EndDate" and T2."status" = ' + "'" + 'A' + "'" + '';
 
   connection.runQuery(res, sql);
 };
