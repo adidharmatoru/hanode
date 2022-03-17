5cb6f508f2ea24c732869f0fe4ff7f56e8929de2
OpsDelta <ops.deltasindo@gmail.com>
2022-03-17 17:39:40 +0700

    Items

--- controllers/ItemsController.js
+++ controllers/ItemsController.js
@@ -12,6 +12,6 @@ exports.masterItems = function(req, res, next) {
   res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
   // var sql = 'select * from oitm T1 Where "ItemName" LIKE ' + "'%"  + req.query.code + "%'" + ' order by T1."ItemCode" limit 5';
-  var sql = 'select distinct "ItemName" as "Name","ItemCode" as "Code","U_DM_CAPACITY" as "Capacity","U_DM_VOLTAGE" as "Voltage","U_DM_PHASE" as "Phase","U_DM_TYPE" as "Type","U_DM_MODEL" as "Model","U_DM_STATUS" as "Status","U_DM_BATTERY" as "Battery","U_DM_COO" as "Coo","U_DM_MEASUREMENT" as "Measurement" from oitm WHERE "ItmsGrpCod" in (' + "'290'" + ',' + "'180'" + ',' + "'273'" + ',' + "'101'" + ',' + "'102'" + ',' + "'144'" + ',' + "'145'" + ')';
+  var sql = 'select distinct "ItemName" as "Name","ItemCode" as "Code","U_DM_CAPACITY" as "Capacity","U_DM_VOLTAGE" as "Voltage","U_DM_PHASE" as "Phase","U_DM_TYPE" as "Type","U_DM_MODEL" as "Model","U_DM_STATUS" as "Status","U_DM_BATTERY" as "Battery","U_DM_COO" as "Coo","U_DM_MEASUREMENT" as "Measurement","ItmsGrpCod" from oitm WHERE "ItmsGrpCod" in (' + "'290'" + ',' + "'180'" + ',' + "'273'" + ',' + "'101'" + ',' + "'102'" + ',' + "'144'" + ',' + "'145'" + ') and "U_DM_CAPACITY" IS NULL and "U_DM_VOLTAGE" IS NULL and "U_DM_PHASE" IS NULL and "U_DM_TYPE" IS NULL and "U_DM_MODEL" IS NULL and "U_DM_STATUS" IS NULL and "U_DM_BATTERY" IS NULL and "U_DM_COO" IS NULL and "U_DM_MEASUREMENT" IS NULL';
   connection.runQuery(res, sql);
 };
