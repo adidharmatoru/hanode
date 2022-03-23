20545e92fb3018c8494d108db4f67d95ff73ca55
OpsDelta <ops.deltasindo@gmail.com>
2022-03-23 09:09:31 +0700

    Items group

--- controllers/ItemsController.js
+++ controllers/ItemsController.js
@@ -11,7 +11,13 @@ exports.masterItems = function(req, res, next) {
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // If needed
   res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
-  // var sql = 'select * from oitm T1 Where "ItemName" LIKE ' + "'%"  + req.query.code + "%'" + ' order by T1."ItemCode" limit 5';
-  var sql = 'select distinct "ItemName" as "Name","ItemCode" as "Code","U_DM_CAPACITY" as "Capacity","U_DM_VOLTAGE" as "Voltage","U_DM_PHASE" as "Phase","U_DM_TYPE" as "Type","U_DM_MODEL" as "Model","U_DM_STATUS" as "Status","U_DM_BATTERY" as "Battery","U_DM_COO" as "Coo","U_DM_MEASUREMENT" as "Measurement","ItmsGrpCod" from oitm WHERE "ItmsGrpCod" in (' + "'290'" + ',' + "'180'" + ',' + "'273'" + ',' + "'101'" + ',' + "'102'" + ',' + "'144'" + ',' + "'145'" + ') and "U_DM_CAPACITY" IS NULL';
+  var sql = 'select distinct "ItemName" as "Name","ItemCode" as "Code","U_DM_CAPACITY" as "Capacity","U_DM_VOLTAGE" as "Voltage","U_DM_PHASE" as "Phase","U_DM_TYPE" as "Type","U_DM_MODEL" as "Model","U_DM_STATUS" as "Status","U_DM_BATTERY" as "Battery","U_DM_COO" as "Coo","U_DM_MEASUREMENT" as "Measurement","ItmsGrpCod" from oitm WHERE "ItmsGrpCod" in (' + req.query.ItmsGrpNam + ') and "U_DM_CAPACITY" IS NULL';
   connection.runQuery(res, sql);
 };
+
+exports.itemsgroup = function(req,res,next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  var sql = 'select oitm."ItmsGrpCod",oitb."ItmsGrpNam" from oitm join oitb on oitm."ItmsGrpCod"=oitb."ItmsGrpCod"';
+  connection.runQuery(res, sql);
+}
--- routes/items.js
+++ routes/items.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const ItemsController = require('../controllers/ItemsController');
 
 router.get('/masterItems',ItemsController.masterItems);
+router.get('/itemsgroup',ItemsController.itemsgroup);
 
 
 module.exports = router;
