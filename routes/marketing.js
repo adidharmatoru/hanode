5a12af933ffbfd2c07415303e361498e139348f1
OpsDelta <ops.deltasindo@gmail.com>
2022-04-11 09:28:43 +0700

    Berita Acara Penagihan

--- routes/marketing.js
+++ routes/marketing.js
@@ -12,6 +12,7 @@ router.get('/contactPerson', MarketingController.contactPerson);
 router.get('/countProject', MarketingController.countProject);
 router.get('/scon', MarketingController.scon);
 router.get('/sconDetail', MarketingController.sconDetail);
+router.get('/sconDetailBA', MarketingController.sconDetailBA);
 router.get('/items', MarketingController.itemCode);
 router.get('/teknisi', MarketingController.teknisi);
 router.get('/CorrectiveMaintenance', MarketingController.CorrectiveMaintenance);
