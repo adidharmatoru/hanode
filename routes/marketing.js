45367683b4dce0be2c4effa986261726add1c21a
OpsDelta <ops.deltasindo@gmail.com>
2022-04-13 15:42:05 +0700

    Corrective Maintenance

--- routes/marketing.js
+++ routes/marketing.js
@@ -15,7 +15,6 @@ router.get('/sconDetail', MarketingController.sconDetail);
 router.get('/sconDetailBA', MarketingController.sconDetailBA);
 router.get('/items', MarketingController.itemCode);
 router.get('/teknisi', MarketingController.teknisi);
-router.get('/CorrectiveMaintenance', MarketingController.CorrectiveMaintenance);
 router.get('/ServiceContract', MarketingController.ServiceContract);
 router.get('/ServiceContractTerm', MarketingController.ServiceContractTerm);
 router.get('/SconItem', MarketingController.SconItem);
