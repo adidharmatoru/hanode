85d6aec2a4c6f608aafed228abc9b0d8b7ce10d7
OpsDelta <ops.deltasindo@gmail.com>
2022-04-16 09:23:16 +0700

    Bug PDCA CM

--- controllers/CorrectiveController.js
+++ controllers/CorrectiveController.js
@@ -22,7 +22,7 @@ exports.salesQuotation = function(req, res, next) {
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // If needed
   res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
-  var sql = 'select T3."DocNum" as "DocNum", T2."TrgetEntry" as "TargetEntry" from qut1 T2 left join oqut T3 on T2."DocEntry"=T3."DocEntry" where T3."DocNum" in (' + req.body.sq + ')';
+  var sql = 'select T3."DocNum" as "DocNum", T2."TrgetEntry" as "TargetEntry" from qut1 T2 left join oqut T3 on T2."DocEntry"=T3."DocEntry" where T3."DocEntry" in (' + req.body.sq + ')';
 
   connection.runQuery(res, sql);
 };
@@ -33,7 +33,7 @@ exports.salesOrder= function(req, res, next) {
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // If needed
   res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
-    var sql = 'select T3."DocNum" as "DocNum",T3."NumAtCard" as "NoPO",SUBSTRING(T3."TaxDate",1,10) as "TaxDate" ,SUBSTRING(T3."DocDueDate",1,10) as "DocDueDate" , SUBSTRING(T3."DocDate",1,10) as "DocDate", T2."BaseEntry" as "BaseEntry" from rdr1 T2 left join ordr T3 on T2."DocEntry"=T3."DocEntry" where T3."DocNum" in (' + req.body.so + ')';
+    var sql = 'select T3."DocNum" as "DocNum",T3."NumAtCard" as "NoPO",SUBSTRING(T3."TaxDate",1,10) as "TaxDate" ,SUBSTRING(T3."DocDueDate",1,10) as "DocDueDate" , SUBSTRING(T3."DocDate",1,10) as "DocDate", T2."BaseEntry" as "BaseEntry" from rdr1 T2 left join ordr T3 on T2."DocEntry"=T3."DocEntry" where T2."BaseEntry" in (' + req.body.so + ')';
 
   connection.runQuery(res, sql);
 };
@@ -42,7 +42,7 @@ exports.Inv = function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // If needed
-  res.setHeader('Access-Control-Allow-Credentials', true); // If needed FORMAT(CAST(T1."U_VIT_TOT" AS DECIMAL(20,6)) , "g16")
+  res.setHeader ('Access-Control-Allow-Credentials', true); // If needed FORMAT(CAST(T1."U_VIT_TOT" AS DECIMAL(20,6)) , "g16")
 
   var sql = 'select distinct T0."DocNum" as "SO_no", T2."BaseEntry", T1."TrgetEntry",T2."DocEntry",T3."DocNum" as "no_inv", T0."U_VIT_TOPY" as "TOP",T1."Dscription",SUBSTRING(T3."DocDate",1,10) as "inv_date",T1."Price" as "Price",T3."DocStatus" as "status" from ordr T0 join rdr1 T1 on T0."DocEntry"=T1."DocEntry" join inv1 T2 on T1."TrgetEntry"=T2."BaseEntry" join oinv T3 on T2."DocEntry"=T3."DocEntry" where T0."DocNum" in (' + req.body.inv + ') order by T0."DocNum"';
 
