7a27d5a50c46999306ab39c875a7469cf0fc94e1
OpsDelta <ops.deltasindo@gmail.com>
2022-05-20 09:30:43 +0700

    Date Posting SO

--- controllers/CorrectiveController.js
+++ controllers/CorrectiveController.js
@@ -66,7 +66,7 @@ exports.delivery= function(req, res, next) {
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // If needed
   res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 
-    var sql = 'select distinct T3."DocNum" as "DocNum", T3."DocEntry" as "DocEntry",T4."SrcvCallID" as "callID",SUBSTRING(T3."CreateDate",1,10) as "CreateDateDO" from dln1 T2 left join odln T3 on T2."DocEntry"=T3."DocEntry" left join scl4 T4 on T2."BaseEntry"=T4."DocAbs" where T3."Series" in ('+"'1963'"+','+"'1615'"+','+"'1346'"+') and T2."DocEntry" in (' + req.body.dlv + ')';
+    var sql = 'select distinct T3."DocNum" as "DocNum", T3."DocEntry" as "DocEntry",T4."SrcvCallID" as "callID",SUBSTRING(T5."DocDate",1,10) as "PostingDateSO",SUBSTRING(T3."CreateDate",1,10) as "CreateDateDO" from dln1 T2 left join odln T3 on T2."DocEntry"=T3."DocEntry" left join scl4 T4 on T2."BaseEntry"=T4."DocAbs" left join rdr1 T5 on T5."BaseEntry"=T4."DocAbs" where T3."Series" in ('+"'1963'"+','+"'1615'"+','+"'1346'"+') and T2."DocEntry" in (' + req.body.dlv + ')';
 
   connection.runQuery(res, sql);
 };
