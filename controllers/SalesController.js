5567e5fec1b4d278bec5f242087bbebc22089f57
OpsDelta <ops.deltasindo@gmail.com>
2022-06-03 15:39:06 +0700

    hOSPITAL

--- controllers/SalesController.js
+++ controllers/SalesController.js
@@ -6,7 +6,7 @@ var hdbext = require('@sap/hdbext');
 
 // start function
 exports.DeliveryGEdanPhilips = function(req, res, next) {
-  var sql = 'Select T0."DocNum" as "DocNum", T0."CardName" as "Name",T0."U_VIT_NPAN" as "address",T1."ItemCode" as "ItemCode", T1."Dscription" as "ItemName",T1."Quantity" as "qty",T2."DistNumber" as "SerialNum", T3."DocNum" as "InvoiceDoc" from odln T0 left join dln1 T1 on T1."DocEntry"=T0."DocEntry" join oinv T3 on T0."DocEntry"=T3."DocEntry" left join osrn T2 on T2."ItemCode"=T1."ItemCode" where T0."CardCode" in ( ' + "'C00646'" + ',' + "'C00646'" + ') and T0."CANCELED" = ' + "'N'" + ' and SUBSTRING(T0."DocDate", 3,2) = ' + "'" + req.query.code + "'" + '';
+  var sql = 'Select T0."DocNum" as "DocNum", T0."CardName" as "Name",T0."U_VIT_NPAN" as "address",T1."ItemCode" as "ItemCode", T1."Dscription" as "ItemName",T1."Quantity" as "qty",T2."DistNumber" as "SerialNum", T3."DocNum" as "InvoiceDoc" from odln T0 left join dln1 T1 on T1."DocEntry"=T0."DocEntry" join oinv T3 on T0."DocEntry"=T3."DocEntry" left join osrn T2 on T2."ItemCode"=T1."ItemCode" where T0."CardCode" in ( ' + "'C00646'" + ',' + "'C00502'" + ') and T0."CANCELED" = ' + "'N'" + ' and SUBSTRING(T0."DocDate", 3,2) = ' + "'" + req.query.code + "'" + '';
 
   connection.runQuery(res, sql);
 };
