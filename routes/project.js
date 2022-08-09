034401a4f48b91a171a078f616b9e6cbe6fedcbd
OpsDelta <ops.deltasindo@gmail.com>
2022-08-09 11:04:39 +0700

    PO

--- routes/project.js
+++ routes/project.js
@@ -21,6 +21,7 @@ router.get('/CMitems', ProjectController.CMitems);
 router.get('/CM', ProjectController.CM);
 router.get('/addwarranty', ProjectController.addwarranty);
 router.get('/itemmonitoring', ProjectController.itemmonitoring);
+router.get('/itemmonitoringPO', ProjectController.itemmonitoringPO);
 
 // adjustedAmmount
 // router.post('/adjustedAmmount', SalesOrderController.adjustedAmmount_create);
