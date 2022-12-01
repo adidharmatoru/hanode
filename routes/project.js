6311997fad7011e1fc016832c6eddf3ea370242a
OpsDelta <ops.deltasindo@gmail.com>
2022-12-01 14:30:17 +0700

    project Budgeting

--- routes/project.js
+++ routes/project.js
@@ -28,7 +28,7 @@ router.get('/scallall', ProjectController.scall);
 router.get('/draftPOProject', ProjectController.budgetingDraftPO);
 router.get('/POProject', ProjectController.budgetingPO);
 router.get('/OPProject', ProjectController.budgetingOP);
-router.get('/budgetingPOCristin', ProjectController.budgetingPOCristin);
+router.get('/PriceSO', ProjectController.PriceSO);
 
 // adjustedAmmount
 // router.post('/adjustedAmmount', SalesOrderController.adjustedAmmount_create);
