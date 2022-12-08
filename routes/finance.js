8a2c8fdbee30cf500645404e738d6d7d7fe86bd2
OpsDelta <ops.deltasindo@gmail.com>
2022-12-08 15:28:23 +0700

    profit and loss

--- routes/finance.js
+++ routes/finance.js
@@ -5,5 +5,6 @@ const FinanceController = require('../controllers/FinanceController');
 router.get('/profitandlossOGdetail', FinanceController.profitandlossOGdetail);
 router.get('/profitandlossOG', FinanceController.profitandlossOG);
 router.get('/profitandlossSO', FinanceController.profitandlossSO);
+router.get('/profitandlossSOdetail', FinanceController.profitandlossSOdetail);
 
 module.exports = router;
