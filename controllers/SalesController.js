9684235fc6977ad82f1cc27f984953f0a99889f3
OpsDelta <ops.deltasindo@gmail.com>
2023-01-24 14:59:53 +0700

    sales

--- controllers/SalesController.js
+++ controllers/SalesController.js
@@ -6,7 +6,7 @@ var hdbext = require('@sap/hdbext');
 
 // start function
 exports.DeliveryGEdanPhilips = function(req, res, next) {
-  var sql = 'Select T0."DocNum" as "DocNum", T0."CardName" as "Name",T0."U_VIT_NPAN" as "address",T1."ItemCode" as "ItemCode", T1."Dscription" as "ItemName",T1."Quantity" as "qty",T2."DistNumber" as "SerialNum", T3."DocNum" as "InvoiceDoc" from odln T0 left join dln1 T1 on T1."DocEntry"=T0."DocEntry" left join oinv T3 on T0."DocEntry"=T3."DocEntry" left join osrn T2 on T2."ItemCode"=T1."ItemCode" where T0."CardCode" in ( ' + "'C00646'" + ',' + "'C00502'" + ') and T0."CANCELED" = ' + "'N'" + ' and SUBSTRING(T0."DocDate", 3,2) = ' + "'" + req.query.code + "'" + '';
+  var sql = 'Select   T0."DocNum" as "DocNum",   T0."CardName" as "Name",   T0."U_VIT_NPAN" as "address",   T1."ItemCode" as "ItemCode",   T1."Dscription" as "ItemName",   T1."Quantity" as "qty",   T2."DistNumber" as "SerialNum",   T3."DocNum" as "InvoiceDoc",  T0."CreateDate" as "TanggalDO",  T0."U_VIT_WGSE" as "WarrantyService",  T0."U_VIT_WGBA" as "WarrantyBattery" from odln T0   left join dln1 T1 on T1."DocEntry" = T0."DocEntry"   left join oinv T3 on T0."DocEntry" = T3."DocEntry"   left join osrn T2 on T2."ItemCode" = T1."ItemCode"   left join oitm T4 on T1."ItemCode" = T4."ItemCode"  left join oitb T5 on T4."ItmsGrpCod" = T5."ItmsGrpCod" where T0."CardCode" in ( ' + "'C00646'" + ',' + "'C00502'" + ',' + "'C0102828'" + ') and T0."CANCELED" = ' + "'N'" + ' and SUBSTRING(T0."DocDate", 3,2) = ' + "'" + req.body.code + "'" + ' and T5."ItmsGrpNam" in (' + req.body.itmgrp + ') ';
 
   connection.runQuery(res, sql);
 };
