90a7b01a099563fbd4c518f731e929bf90502706
OpsDelta <ops.deltasindo@gmail.com>
2023-01-25 14:59:27 +0700

    hospital rs

--- routes/sales.js
+++ routes/sales.js
@@ -3,5 +3,6 @@ var router = express.Router();
 const SalesController = require('../controllers/SalesController');
 
 router.get('/DeliveryGEdanPhilips', SalesController.DeliveryGEdanPhilips);
+router.get('/DeliveryRS', SalesController.DeliveryRS);
 
 module.exports = router;
