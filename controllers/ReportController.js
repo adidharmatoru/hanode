b723d83b74529951c889d52d3418c14a6a569a9c
OpsDelta <ops.deltasindo@gmail.com>
2023-08-03 16:58:13 +0700

    report

--- controllers/ReportController.js
+++ controllers/ReportController.js
@@ -345,6 +345,6 @@ var sql = 'select   distinct O1."DocNum" as "DocNum_SQ",  O1."CardName" as "Cust
 
 exports.sotidakadasq = function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  var sql = 'select   r1."DocNum",   r1."CardName",   r1."NumAtCard",   Q1."DocEntry" from   ordr r1   left join rdr1 r2 on r1."DocEntry" = r2."DocEntry"   left join qut1 Q1 on r2."BaseEntry" = Q1."DocEntry"   and r2."BaseLine" = Q1."LineNum" or Q1."DocEntry" = r2."TrgetEntry" and r2."BaseLine" = Q1."LineNum"where   r1."OwnerCode" = 656   and r2."BaseEntry" is null and Q1."DocEntry" is null or r2."TrgetEntry" is null and  r1."OwnerCode" = 656 and Q1."DocEntry" is null order by   r1."DocNum"';
+  var sql = 'select   r1."DocNum",   r1."CardName",   r1."NumAtCard",   Q1."DocEntry",r2."TrgetEntry" , r2."BaseEntry"from   ordr r1   left join rdr1 r2 on r1."DocEntry" = r2."DocEntry"   left join qut1 Q1 on r2."BaseEntry" = Q1."DocEntry"   and r2."BaseLine" = Q1."LineNum" or Q1."DocEntry" = r2."TrgetEntry" and r2."BaseLine" = Q1."LineNum"where  r2."TrgetEntry" is null and Q1."DocEntry" is null and r1."OwnerCode" = 656 and r1."CANCELED" = ' + "'" + 'N' + "'" + ' or  r2."TrgetEntry" = '0' and Q1."DocEntry" is null and r1."OwnerCode" = 656 and r1."CANCELED" = ' + "'" + 'N' + "'" + ' order by   r1."DocNum";';
   connection.runQuery(res, sql)
 }
