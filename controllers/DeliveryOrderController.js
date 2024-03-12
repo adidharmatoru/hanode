56bbd4098bfd9e04b9b62001fd2e324c7ade8435
OpsDelta <ops.deltasindo@gmail.com>
2024-03-12 08:31:49 +0700

    Check Warranty

--- controllers/DeliveryOrderController.js
+++ controllers/DeliveryOrderController.js
@@ -85,3 +85,14 @@ exports.detaildelivery = function(req, res)
   var sql ='select   D1."DocNum",   D1."DocStatus",   D2."ItemCode",   D2."Dscription",   D2."ItemType", D1."CANCELED" from   odln D1   join dln1 D2 on D1."DocEntry" = D2."DocEntry"where	D1."DocNum" = ' + "'" + req.query.code + "'" + '';
   connection.runQuery(res, sql);
 }
+
+exports.checkwarranty = function(req, res)
+{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+    var sql ='select   S1."delivery",   S1."status",   S1."deliveryNo",   N1."Project",  S1."internalSN",  N2."DocDate",  N2."DocDueDate",  N2."U_VIT_WGSE" as "WarrantyServ",  N2."U_VIT_WGSA" as "WarrantySpare",  N2."U_VIT_WGBA" as "WarrantyBat"from   oins S1   left join DLN1 N1 on S1."delivery" = N1."DocEntry"   left join odln N2 on N1."DocEntry" = N2."DocEntry"where   S1."status" = 'A'   and N1."Project" is not null';
+    connection.runQuery(res, sql);
+}
